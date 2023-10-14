97b25a590072ab7ec3e82f518727626422aacf3f
BranBayou <berhan.baye@gmail.com>
2023-10-14 13:18:51 +0300

    Add app.js file

---
+++ dist/app.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const list = document.querySelector(".list");
+const addBtn = document.querySelector(".add-btn");
+
+body.addEventListener("click", (e) => {
+  console.log(e.target);
+}); 
