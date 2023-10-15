022c7c3781584320688726347a60e95a6ecc6ec5
BranBayou <berhan.baye@gmail.com>
2023-10-15 09:08:06 +0300

    Add more styling

--- dist/app.js
+++ dist/app.js
@@ -11,6 +11,7 @@ function addTodo() {
   } else {
     let radio = document.createElement("radio");
     let li = document.createElement("li");
+    li.classList.add("flex", "justify-between", "my-3", "bg-fuchsia-300", "p-2", "rounded-xl", "text-violet-700");
     li.innerHTML = `<div class="left"><input class="radio" type="radio" id="checked" name="fav_language" value="">
     ${todo.value}
     </div>
